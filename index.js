31e736ebd34bb3be7fa985c20d836f1af9d06863
reez37 <80540310+reez37@users.noreply.github.com>
2023-04-24 17:18:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,31 +21,31 @@ app.listen(8000 || process.env.PORT,()=>{
     console.log("webhook is listening")
 });
 //httpget
-app.get("/webhook",(req,res)=>{
-    let mode = req.query["hub.mode"];
-    let challenge = req.query["hub.challenge"];
-    let tokens = req.query["hub.verify_token"];
+// app.get("/webhook",(req,res)=>{
+//     let mode = req.query["hub.mode"];
+//     let challenge = req.query["hub.challenge"];
+//     let tokens = req.query["hub.verify_token"];
 
     
 
-    if(mode && token){
-        if(mode === "subscribe" && tokens === mytoken){
-            res.status(200).send(challenge);
-            console.log("reez is on")
-        }else{
-            res.status(400)
-        }
-    }
+//     if(mode && token){
+//         if(mode === "subscribe" && tokens === mytoken){
+//             res.status(200).send(challenge);
+//             console.log("reez is on")
+//         }else{
+//             res.status(400)
+//         }
+//     }
 
     
-});
+// });
 
 //httppost openai
 async function api(){
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{role:"system",content:"you are a politician from India, representing the Indian National Congress Party and standing as a candidate for the MLA post in the upcoming Karnataka election for the first time and people are asking you a question as a part of campaign your job is to answer and convince them to vote for you.reply them in short as possible"},
-            {role: "user", content: msg}
+            {role: "user", content: "hello"}
         ]
       });
       response = completion.data.choices[0].message.content;
@@ -58,42 +58,42 @@ async function api(){
 
  api()
 
-//httppost whatsapp
-app.post("/webhook",(req,res) =>{
-    let body_param = req.body;
-    console.log("posting")
-    if(body_param.object){
-        if(body_param.entry ){
-                let phone_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-                let from = body_param.entry[0].changes[0].value.messages[0].from;
-                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-                console.log(phone_no_id)
-                console.log(from)
-                console.log(msg_body)
+// //httppost whatsapp
+// app.post("/webhook",(req,res) =>{
+//     let body_param = req.body;
+//     console.log("posting")
+//     if(body_param.object){
+//         if(body_param.entry ){
+//                 let phone_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+//                 let from = body_param.entry[0].changes[0].value.messages[0].from;
+//                 let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+//                 console.log(phone_no_id)
+//                 console.log(from)
+//                 console.log(msg_body)
                
-                axios({
-                    method:"POST",
-                    url:"https://graph.facebook.com/v16.0/"+phone_no_id+"/messages?access_token="+token,
-                    data:{
-                        "messaging_product": "whatsapp",    
-                        "recipient_type": "individual",
-                        "to": from,
-                        "type": "text",
-                        "text": {
-                            "preview_url": false,
-                            "body": response
-                        },
-                        headers:{
-                            "Content-Type":"application/json"
-                        }
-                    }
-                });
-                res.sendStatus(200);
-            }else{
-                res.sendStatus(404);
-            }
-    }
-})
+//                 axios({
+//                     method:"POST",
+//                     url:"https://graph.facebook.com/v16.0/"+phone_no_id+"/messages?access_token="+token,
+//                     data:{
+//                         "messaging_product": "whatsapp",    
+//                         "recipient_type": "individual",
+//                         "to": from,
+//                         "type": "text",
+//                         "text": {
+//                             "preview_url": false,
+//                             "body": response
+//                         },
+//                         headers:{
+//                             "Content-Type":"application/json"
+//                         }
+//                     }
+//                 });
+//                 res.sendStatus(200);
+//             }else{
+//                 res.sendStatus(404);
+//             }
+//     }
+// })
 
 
 
